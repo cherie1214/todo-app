4ce25f02ced8c8b13f478546a18d811fd09b5ca5
Riyoung Choi <cheriey1214@gmail.com>
2018-05-22 16:34:34 +0900

    Coding the UI part1

--- front-end-study/native-study/todo-app/App.js
+++ front-end-study/native-study/todo-app/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const { width, height } = Dimensions.get("window")
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>To do app!!</Text>
+        <StatusBar barStyle="light-content"/>
+        <Text style={styles.tit}>TO DO LIST</Text>
+        <View style={styles.card}>
+            <TextInput style={styles.input} placeholder={"New To do"}></TextInput>
+        </View>
       </View>
     );
   }
@@ -14,8 +20,35 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fda33e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  tit: {
+      marginTop: 70,
+      marginBottom: 20,
+      color: '#fff',
+      fontSize: 30,
+      fontWeight: '200',
+  },
+  card: {
+      backgroundColor: '#fff',
+      flex: 1,
+      width: width - 40,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      ...Platform.select({
+          ios: {
+              shadowColor:'rgb(50, 50, 50)',
+              shadowOpacity: 0.5,
+              shadowRadius: 5,
+              shadowOffset: {
+                  width: 1,
+                  height: 1,
+              }
+          },
+          android: {
+              elevation: 3,
+          }
+      })
   },
 });
