53379f836f407169cefda392dead49231bfdc72b
Riyoung Choi <cheriey1214@gmail.com>
2018-05-27 15:29:33 +0900

    Styling the To Do Component part 1

--- front-end-study/native-study/todo-app/ToDo.js
+++ front-end-study/native-study/todo-app/ToDo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const { width, height } = Dimensions.get("window")
 
@@ -10,17 +11,52 @@ export default class ToDo extends Component {
     }
 
     render(){
-        const { isCompleted } = this.state;
+        const { isCompleted, isEditing } = this.state;
 
         return(
             <View style={styles.container}>
-                <TouchableOpacity onPress={this._toggleComplete}>
-                    <View style={[
-                        styles.circle,
-                        isCompleted ? styles.completedCircle : styles.uncompletedCircle]}
-                    />
-                </TouchableOpacity>
-                <Text style={styles.text}> Todo will be here</Text>
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        <View style={[
+                            styles.circle,
+                            isCompleted ? styles.completedCircle : styles.uncompletedCircle]}
+                        />
+                    </TouchableOpacity>
+                    <Text style={[
+                        styles.text,
+                        isCompleted ? styles.completedText : styles.uncompletedText
+                        ]}
+                    >
+                        Todo will be here
+                    </Text>
+                </View>
+                <View style={isCompleted ? styles.hideActions : styles.showActions}>
+                    { isEditing ?
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._finishEditing}>
+                                <View style={styles.actionContainer}>
+                                    {/*<Text style={styles.actionText}>V</Text>*/}
+                                    <Feather color="green" size={25} name="check" style={styles.actionIcon} />
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                     :
+                         <View style={styles.actions}>
+                             <TouchableOpacity onPressOut={this._startEditing}>
+                                 <View style={styles.actionContainer}>
+                                     {/*<Text style={styles.actionText}>수정</Text>*/}
+                                     <Feather color="#666" size={22} name="edit-2" style={styles.actionIcon} />
+                                 </View>
+                             </TouchableOpacity>
+                             <TouchableOpacity>
+                                 <View style={styles.actionContainer}>
+                                     {/*<Text style={styles.actionText}>삭제</Text>*/}
+                                     <Feather color="#e83737" size={22} name="delete" style={styles.actionIcon} />
+                                 </View>
+                             </TouchableOpacity>
+                         </View>
+                     }
+                </View>                
             </View>
         )
     }
@@ -32,6 +68,16 @@ export default class ToDo extends Component {
             }
         })
     }
+    _startEditing = () => {
+         this.setState({
+             isEditing: true,
+         })
+    }
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false,
+        })
+    }
 }
 
 const styles = StyleSheet.create({
@@ -41,24 +87,48 @@ const styles = StyleSheet.create({
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    column: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: width / 2,
+        justifyContent: 'space-between',
     },
     circle: {
         marginRight: 10,
         width: 30,
         height: 30,
         borderRadius: 15,
-        borderColor: 'red',
+        borderColor: '#fda33e',
         borderWidth: 3,
     },
     completedCircle: {
         borderColor: '#bbb',
     },
     uncompletedCircle: {
-        borderColor: '#f23657',
+        borderColor: '#f19023',
     },
     text: {
         fontSize: 20,
         fontWeight: '600',
         marginVertical: 15 ,
+    },
+    completedText: {
+        color: '#bbb',
+        textDecorationLine: 'line-through',
+    },
+    uncompletedText: {
+        color: '#353839',
+    },
+    actions: {
+        flexDirection: 'row',
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 7,
+    },
+    hideActions: {
+        height: 0,
     },
 })
