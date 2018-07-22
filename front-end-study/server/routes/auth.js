a0ac8fde086f7b2c4674e163cbee14a67c7f2da3
Riyoung Choi <cheriey1214@gmail.com>
2018-07-22 19:17:38 +0900

    #9. implement the function of 'Sign In'

--- front-end-study/server/routes/auth.js
+++ front-end-study/server/routes/auth.js
@@ -8,21 +8,79 @@ router.get('/', (req, res) => {
         success: true
     });
 });
-
-router.post('/signUp', (req, res) => {
-//   console.log(req.body)
-    let user = new Users({
-        id: req.body.id,
-        pw: req.body.pw,
-        nickname: req.body.nickname,
-    });
-    user.save((err) => {
-        if(err) {
-            res.json({ status: "FAILED"})
+router.post('/signIn', (req, res) => {
+    console.log(req.body);
+    
+    Users.findOne({id: req.body.id}, (err, doc) => {
+        if(err){
+            res.json({
+                status: "SIGNIN_ERROR"
+            })
             return false;
         }
-        res.json({ status: "SUCCESS" })
+        // 아이디가 없을 경우
+        if(!doc || doc === {}){
+            res.json({
+                status: "SIGNIN_FAILED"
+            })
+            return false;
+        }
+        // 비밀번호가 일치하지 않을 경우
+        if(doc.pw !== req.body.pw){
+            res.json({
+                status: "SIGNIN_FAILED"
+            })
+            return false;
+        }
+        // 성공!
+        res.json({
+            status: "SIGNIN_SUCCESS",
+            nickname: doc.nickname,
+            id: doc.id,
+        })
+        return false;
     })
 });
+
+router.post('/signUp', (req, res) => {
+    // console.log(req.body)
+
+    // 중복체크
+    Users.findOne({ $or : [
+            {id: req.body.id},
+            {nickname: req.body.nickname},
+    ]}, (err, doc) => {
+        // console.log(doc)
+        if(doc && doc.id && doc.id === req.body.id){
+            res.json({
+                status: "SIGNUP_ID_DUPLICATED"
+            })
+            return false;
+        }
+        if(doc && doc.nickname && doc.nickname === req.body.nickname){
+            res.json({
+                status: "SIGNUP_NICKNAME_DUPLICATED"
+            })
+            return false;
+        }
+
+        // 아이디 생성
+        let user = new Users({
+            id: req.body.id,
+            pw: req.body.pw,
+            nickname: req.body.nickname,
+        });
+        user.save((err) => {
+            if(err) {
+                res.json({ status: "SIGNUP_FAILED"})
+                return false;
+            }
+            res.json({ status: "SIGNUP_SUCCESS" })
+        })
+    })
+
+});
+
+
 
 module.exports = router;
